85a9c3ea1c261dd0e6b788372977097410571884
bartek <bartekmtb@gmail.com>
2026-03-11 21:18:57 +0000

    fix: update home view to retrieve SOLANA_CA from environment variables

--- django_base/views.py
+++ django_base/views.py
@@ -1,13 +1,14 @@
+import os
+
 from django.shortcuts import render
 from django.http import Http404
-from .models import SiteSettings
 from service_apps.battles.models import Battle
 
 
 def home(request):
-    settings = SiteSettings.get()
+    solana_ca = os.environ.get('SOLANA_CA', '')
     battles = Battle.objects.select_related('posted_by', 'player_a', 'player_b', 'winner').order_by('-pk')[:12]
-    return render(request, 'home.html', {'solana_ca': settings.solana_ca, 'battles': battles})
+    return render(request, 'home.html', {'solana_ca': solana_ca, 'battles': battles})
 
 def error_404_view(request, exception=None):
     """Render custom 404 page."""
